22b3defda8d2137d255b99658dc234690639d0d6
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-11-09 21:49:54 -0300

    adicionando banco

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,17 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/Pessoa"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Pessoa } from "./entity/Pessoa";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  host: "ec2-54-86-224-85.compute-1.amazonaws.com",
+  port: 5432,
+  username: "fqqtnregrxrlgg",
+  password: "f31aa5561ca0e5f88db3a1f0211ebfbf657cd3b17a1b40116dc5374bc433df4e",
+  database: "d9rrg7vsrbvrv2",
+  synchronize: true,
+  logging: false,
+  entities: [Pessoa],
+  migrations: [],
+  subscribers: [],
+});
