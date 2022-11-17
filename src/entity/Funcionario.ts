94e0bdb6cda0b53fc3f1757fcb786422cca39255
Matheus José Ferreira de Aguiar <matheusjfa@outlook.com>
2022-11-16 22:38:24 -0300

    adding two new classes

---
+++ src/entity/Funcionario.ts
@@ -0,0 +1,20 @@
+import { Entity, Column } from "typeorm"
+import { Pessoa } from "./Pessoa";
+
+@Entity("funcionarios")
+export class Funcionario extends Pessoa {
+  @Column()
+  funcao: string;
+
+  constructor(nome: string, email: string, codNac: string, funcao: string) {
+    super(nome, email, codNac);
+    this.funcao = funcao;
+  }
+
+  alterarFuncionario = (nome: string, email: string, codNac: string, funcao: string): void => {
+    this.nome = nome;
+    this.email = email;
+    this.codNac = codNac;
+    this.funcao = funcao;
+  }
+}
