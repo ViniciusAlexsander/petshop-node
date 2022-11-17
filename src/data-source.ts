f93f6961b9cc63c6c770a5383340bb0c35f06870
Matheus José Ferreira de Aguiar <matheusjfa@outlook.com>
2022-11-16 22:05:16 -0300

    Changes

--- src/data-source.ts
+++ src/data-source.ts
@@ -18,5 +18,5 @@ export const AppDataSource = new DataSource({
   entities: [Cidade, Endereco, Estado, Pessoa, Telefone],
   migrations: [],
   subscribers: [],
-  ssl: true,
+  ssl: { rejectUnauthorized: false }
 });
