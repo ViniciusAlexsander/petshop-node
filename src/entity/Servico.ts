181f5581d1747bc9fc629ead6e62dcdb5fcfa438
CelsonF <celsondev@outlook.com>
2022-11-19 11:09:41 -0300

    adjust servico

--- src/entity/Servico.ts
+++ src/entity/Servico.ts
@@ -1,9 +1,22 @@
-import { Entity, Column, ManyToOne, OneToMany } from "typeorm"
+import { Pagamento } from './Pagamento';
+import { Produto } from 'entity/Produto';
+import { Pet } from 'entity/Pet';
+import { Entity, Column, ManyToOne, OneToMany, OneToOne } from "typeorm"
 import Base from "./Base"
 
 
 @Entity("servico")
 export class Servico extends Base {
+
+		@OneToMany(() => Produto, produto => produto.id)
+		produto: Produto;
+
+		@OneToMany(() => Pet, pet => pet.id)
+		pet: Pet;
+
+		@OneToOne(() => Pagamento, pagamento => pagamento.id)
+		pagamento: Pagamento;
+
     @Column()
     dataEntrada: Date;
 
